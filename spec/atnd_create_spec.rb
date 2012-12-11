d9685523e374a6b5b6e50a7a9ed8e580283845db
ginkouno <kouno.ruby@gmail.com>
2012-12-11 21:05:58 +0900

    add test

--- spec/atnd_create_spec.rb
+++ spec/atnd_create_spec.rb
@@ -1,16 +1,27 @@
 # -*- encoding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'webmock/rspec'
 
 describe "atnd create" do
-  context 'login type error' do
-    it "raise error" do
-      lambda{ AtndCreate::Event.new("account", "password", {:login_type => "hoge"}) }.should raise_error(AtndCreate::AtndCreateException)
+  describe 'initialize' do
+    context 'login type error' do
+      it "raise error" do
+        lambda{ AtndCreate::Event.new("account", "password", {:login_type => "hoge"}) }.should raise_error(AtndCreate::AtndCreateException)
+      end
+    end
+
+    context 'login type is correct' do
+      it "raise not error" do
+        lambda{ AtndCreate::Event.new("account", "password", {:login_type => :google}) }.should_not raise_error
+      end
     end
   end
 
-  context 'login type is correct' do
-    it "raise not error" do
-      lambda{ AtndCreate::Event.new("account", "password", {:login_type => :google}) }.should_not raise_error
+  describe 'login_google' do
+    context 'success' do
+    end
+
+    context 'failure' do
     end
   end
 end
